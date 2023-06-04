1767764b003871ffb13433161255c49f3121fc8a
gricana <alex2904.goryachev@yandex.com>
2023-06-04 04:08:52 +0300

    Added required vars list

--- deploy/settings.py
+++ deploy/settings.py
@@ -18,3 +18,8 @@ YC_KMS_ENDPOINT = {
     'decrypt': 'https://kms.yandex/kms/v1/keys/{keyId}:decrypt',
 }
 
+REQUIRED_VARIABLES = {
+    'SETTINGS': ['DJANGO_SETTINGS_MODULE'],
+    'DB': ['DATABASE_NAME', 'DATABASE_USER', 'DATABASE_PASSWORD'],
+    'ADMIN': ['ADMIN_USERNAME', 'ADMIN_EMAIL', 'ADMIN_PASSWORD']
+}
