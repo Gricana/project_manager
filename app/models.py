7323e5fd8bf8bf3ab52d932d943e22df757e6e49
Gricana <alex2904.goryachev@yandex.com>
2024-07-25 18:56:41 +0300

    Added comments to API models

--- app/models.py
+++ app/models.py
@@ -3,6 +3,15 @@ from run import db
 
 
 class User(db.Model):
+    """
+    User model.
+
+    Attributes:
+        id (int): Unique user identifier
+        username (str): Username
+        tg_id (int): Unique Telegram user ID
+    """
+
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -10,19 +19,46 @@ class User(db.Model):
     tg_id = db.Column(db.Integer, unique=True)
 
     def get_projects(self):
+        """
+        Retrieving all user projects
+
+        :return: List of user projects
+        """
         return self.projects.all()
 
     def save(self):
+        """
+        Saving the user in the database
+        """
         db.session.add(self)
         db.session.commit()
 
     def to_dict(self):
+        """
+        Convert the user object to a dictionary
+
+        :return: Dictionary with user data
+        """
         return {
             'username': self.username
         }
 
 
 class Project(db.Model):
+    """
+    Project model
+
+    Attributes:
+        id (int): Unique identifier for the project
+        name (str): Project name
+        description (str): Description of the project
+        created_at (datetime): The date and time the project was created
+        updated_at (datetime): The date and time the project was last updated
+        user_id (int): Identifier of the user who owns the project
+        user (User): Link to the user model
+        status (str): Project status (Running or Stopped)
+    """
+
     __tablename__ = 'projects'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -35,21 +71,39 @@ class Project(db.Model):
     status = db.Column(db.Enum('Запущен', 'Остановлен'), default='Остановлен', nullable=False)
 
     def save(self):
+        """
+        Saving the project in the database
+        """
         db.session.add(self)
         db.session.commit()
 
     def delete(self):
+        """
+        Removing a project from the database
+        """
         db.session.delete(self)
         db.session.commit()
 
     def update(self, name=None, description=None, status=None):
+        """
+        Update project data
+
+        :param name: New project name
+        :param description: New description of the project
+        :param status: New project status
+        """
         for key, value in locals().items():
             if key != 'self' and value is not None:
                 setattr(self, key, value)
-            self.updated_at = datetime.utcnow()
-            db.session.commit()
+        self.updated_at = datetime.utcnow()
+        db.session.commit()
 
     def to_dict(self):
+        """
+        Convert a project object to a dictionary
+
+        :return: Dictionary with project data
+        """
         return {
             'name': self.name,
             'description': self.description,
