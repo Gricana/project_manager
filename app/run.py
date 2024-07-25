c829cbb5e9fb3bca20fc9789ac131b640a61cbdd
Gricana <alex2904.goryachev@yandex.com>
2024-07-25 18:57:27 +0300

    Added comments to run API script

--- app/run.py
+++ app/run.py
@@ -5,19 +5,18 @@ from config import Config
 
 
 app = Flask(__name__)
-app.config.from_object(Config())
+app.config.from_object(Config())  # Load configuration
 
-# Databases init
+# Initialize the DB
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
+# Register controllers
 from controllers import api
-# URL register
 app.register_blueprint(api, url_prefix='/api')
-for rule in app.url_map.iter_rules():
-    print(rule.rule, rule.endpoint)
 
+# for rule in app.url_map.iter_rules():
+#     print(rule.rule, rule.endpoint)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=6699)
