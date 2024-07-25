6c8367b232d23f416db69447d8a4086f2056e5cd
Gricana <alex2904.goryachev@yandex.com>
2024-07-25 18:33:59 +0300

    Added comments to API configuration

--- app/config.py
+++ app/config.py
@@ -2,21 +2,31 @@ import os
 
 
 class Config:
+    """
+    Class for storing API configuration parameters.
+    """
 
     def __init__(self):
         self.DEBUG = False
+
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
+
+        # MySQL database connection parameters
         self.SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{username}:{password}@{hostname}/{dbname}'.format(
             username=os.environ.get('HOST_USER'),
             password=os.environ.get('HOST_PASSWORD'),
             hostname=os.environ.get('DB_HOST'),
             dbname=os.environ.get('DB_NAME')
         )
+
         self.SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+        # SSL options
         self.SSL_ENABLED = True
         self.SSL_CERTIFICATE = os.environ.get('SSL_CERT_PATH')
         self.SSL_PRIVATE_KEY = os.environ.get('SSL_KEY_PATH')
+
+        # Security settings
         self.SERVER_NAME = 'ewdbot.com'
         self.PREFERRED_URL_SCHEME = 'https'
         self.SESSION_COOKIE_SECURE = True
